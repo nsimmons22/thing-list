2a9b355f6ec9fb41f6e86f4fa0e49b35e30e98f4
Nicholas Simmons <nssimmons2015@gmail.com>
2017-05-24 13:28:35 -0400

    Added changes to code

--- src/Thing.js
+++ src/Thing.js
@@ -1,22 +1,46 @@
-import React from 'react'
-import './Thing.css'
+import React, { Component } from 'react'
+import ContentEditable from 'react-contenteditable'
 
-const Thing = ({ thing }) => {
-  return (
-    <li className="Thing">
-      <input type="checkbox" value="on" />
-      <div className="details">
-        <div className="name">
-          {thing.name}
+import './Thing.css'
+import Actions from './Actions'
+
+class Thing extends Component {
+  componentDidMount() {
+    this.nameInput.htmlEl.focus()
+  }
+
+  updateName = (ev) => {
+    const { thing, saveThing } = this.props
+    thing.name = ev.target.value
+    saveThing(thing)
+  }
+
+  blurOnEnter = (ev) => {
+    if (ev.key === 'Enter') {
+      ev.preventDefault()
+      ev.target.blur()
+    }
+  }
+
+  render() {
+    const { thing, removeThing } = this.props
+
+    return (
+      <li className="Thing">
+        <input type="checkbox" value="on" />
+        <div className="details">
+          <ContentEditable
+            className="name"
+            html={thing.name}
+            onChange={this.updateName}
+            onKeyPress={this.blurOnEnter}
+            ref={input => this.nameInput = input}
+          />
+          <Actions thing={thing} removeThing={removeThing} />
         </div>
-        <span className="actions">
-          <button className="remove">
-            <i className="fa fa-trash-o"></i>
-          </button>
-        </span>
-      </div>
-    </li>
-  )
+      </li>
+    )
+  }
 }
 
 export default Thing
